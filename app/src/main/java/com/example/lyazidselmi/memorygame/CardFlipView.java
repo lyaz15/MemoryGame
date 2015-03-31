85fcdc527c31c120fa455a3567c1e4da34d49c76
lyaz <lyrab@hotmail.fr>
2015-03-31 17:44:48 +0200

    Handling animations and card flipping (basic version, no timer yet)

--- app/src/main/java/com/example/lyazidselmi/memorygame/CardFlipView.java
+++ app/src/main/java/com/example/lyazidselmi/memorygame/CardFlipView.java
@@ -1,8 +1,10 @@
 package com.example.lyazidselmi.memorygame;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.GridView;
 import android.widget.ImageView;
 
 /**
@@ -15,11 +17,18 @@ public class CardFlipView extends ImageView implements Animation.AnimationListen
     private Animation from_middle;
     private boolean canFlip;
     private int headCardId;
+    private String log;
+    private boolean animStarted;
+    static int count = 0;
+    private int anim = 0;
     public CardFlipView(Context context, int headCardId){
         super(context);
         isShowingHeadCard = false;
         this.headCardId = headCardId;
         canFlip = true;
+        this.setImageResource(R.drawable.back);
+        this.setLayoutParams(new GridView.LayoutParams(-10, -10));
+        this.setScaleType(ImageView.ScaleType.CENTER_CROP);
         to_middle = AnimationUtils.loadAnimation(context,R.anim.to_middle);
         to_middle.setAnimationListener(this);
         from_middle = AnimationUtils.loadAnimation(context,R.anim.from_middle);
@@ -35,13 +44,17 @@ public class CardFlipView extends ImageView implements Animation.AnimationListen
     }
 
 
+
     @Override
     public void onAnimationStart(Animation animation) {
-
+        animStarted = true;
     }
 
     @Override
     public void onAnimationEnd(Animation animation) {
+        if(!animStarted)
+            return;
+        animStarted = false;
         if (animation==to_middle) {
             if (isShowingHeadCard) {
                 this.setImageResource(R.drawable.back);
@@ -55,6 +68,12 @@ public class CardFlipView extends ImageView implements Animation.AnimationListen
         } else {
             isShowingHeadCard=!isShowingHeadCard;
         }
+        count++;
+        if(count == 4){
+            ((GameActivity)getContext()).checkCards();
+            count = 0;
+        }
+
     }
 
     @Override
@@ -62,7 +81,22 @@ public class CardFlipView extends ImageView implements Animation.AnimationListen
 
     }
 
+    public boolean isShowingHeadCard(){
+        return isShowingHeadCard;
+    }
     public void canFlip(boolean canflip){
         this.canFlip = canflip;
+    }
+
+    public int getHeadCardId(){
+        return headCardId;
+    }
+
+    public void reset(){
+        if(!isShowingHeadCard())
+            return;
+        this.setImageResource(R.drawable.back);
+        isShowingHeadCard = false;
+        canFlip = true;
     }
 }
--- app/src/main/java/com/example/lyazidselmi/memorygame/GameActivity.java
+++ app/src/main/java/com/example/lyazidselmi/memorygame/GameActivity.java
@@ -5,15 +5,22 @@ import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.GridView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView;
 import android.view.View;
+import android.widget.ImageView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -21,122 +28,97 @@ import java.util.Objects;
  */
 public class GameActivity extends Activity{
 
-    private GridView gameView;
-    private final ImageAdapter adapter;
-    private Integer firstClick, secondClick;
-    private final static int END_GAME = 10;
-    private boolean restart;
-    private String dialog_message;
-    private Integer[] cardIds;
-    static  final Integer[] tails = new Integer[]{
-            R.drawable.back
-    };
-    static final Integer[] heads = new Integer[]{
-            R.drawable.image1,
-            R.drawable.image2
-    };
 
-    private CardFlipView[] cardViews;
+    private GridView gameView;
+    private CardFlipAdapter cardAdapter;
+    private boolean win;
+    private  CardFlipView[] cards;
+    private int nbValidCards;
+    private CardFlipView previousCard,currentCard;
 
     public GameActivity(){
         super();
-        cardIds = new Integer[] {
-                R.drawable.back,
-                R.drawable.back,
-                R.drawable.back,
-                R.drawable.back
-        };
-        restart = false;
-        firstClick = -1;
-        secondClick = -1;
-        adapter = new ImageAdapter(this,cardIds);
+        win = false;
+        nbValidCards = 0;
     }
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game);
-
         gameView = (GridView) findViewById(R.id.gameView1);
-
-        gameView.setAdapter(adapter);
+        cards = new CardFlipView[]{
+                new CardFlipView(this,R.drawable.image1),
+                new CardFlipView(this,R.drawable.image1),
+                new CardFlipView(this,R.drawable.image2),
+                new CardFlipView(this,R.drawable.image2),
+                new CardFlipView(this,R.drawable.image3),
+                new CardFlipView(this,R.drawable.image3)
+        };
+        cardAdapter = new CardFlipAdapter(cards);
+        gameView.setAdapter(cardAdapter);
+        //gameView.setAdapter(adapter);
         gameView.setOnItemClickListener(new OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
-
-                ((CardFlipView)adapter.getView(position,null,gameView)).flipView();
-                adapter.notifyDataSetChanged();
-                gameView.setAdapter(adapter);
-                gameView.invalidateViews();
-                /**
-                if(position < 2)
-                    cardIds[position] = heads[0];
+            public void onItemClick(AdapterView<?> parent, final View v,int position, long id) {
+                if(previousCard == null){
+                    previousCard = (CardFlipView)v;
+                    previousCard.flipView();
+                }
                 else
-                    cardIds[position] = heads[1];
-
-                if(firstClick == -1){
-                    firstClick = position;
+                {
+                    currentCard = (CardFlipView)v;
+                    if(currentCard.isShowingHeadCard())
+                        return;
+                    currentCard.flipView();
                 }
-                else {
-                    secondClick = position;
-                    if((firstClick < 2 && secondClick < 2) || (firstClick >= 2 && secondClick >= 2))
-                        dialog_message = "Congratulations you win :)";
-                        // Toast.makeText(getApplicationContext(),
-                        //       "You win", Toast.LENGTH_LONG).show();
-                    else
-                        dialog_message = "OOOPS you lose !";
-                    //Toast.makeText(getApplicationContext(),
-                    //      "You lose", Toast.LENGTH_SHORT).show();
-                    dialog_message += " \nTry again ?";
-                    showDialog(END_GAME);
-
-                }
-                Log.i("Memory Game","here");
-                adapter.notifyDataSetChanged();
-                gameView.setAdapter(adapter);
-                gameView.invalidateViews();**/
+                cardAdapter.notifyDataSetChanged();
             }
         });
     }
-
-    @Override
-    protected Dialog onCreateDialog(int id){
-        switch (id) {
-            case END_GAME:
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setMessage(dialog_message);
-                builder.setCancelable(true);
-                builder.setPositiveButton("Yes", new OkOnClickListener());
-                builder.setNegativeButton("No", new CancelOnClickListener());
-                AlertDialog dialog = builder.create();
-                dialog.show();
-        }
-        return super.onCreateDialog(id);
+    void showDialog() {
+        int title;
+        if(win)
+            title = R.string.alert_dialog_win_title;
+        else
+            title = R.string.alert_dialog_lose_title;
+        DialogFragment newFragment = YesNoDialogFragment.newInstance(title);
+        newFragment.show(getFragmentManager(), "dialog");
     }
 
-    private void handleDialog(){
+    public void handleDialog(boolean restart){
         if(restart){
-            cardIds[firstClick] = tails[0];
-            cardIds[secondClick] = tails[0];
-            firstClick = -1;
-            secondClick = -1;
-            restart = false;
-            adapter.notifyDataSetChanged();
-            gameView.setAdapter(adapter);
-            gameView.invalidateViews();
+            this.reset();
         }else
             this.finish();
     }
-    private final class CancelOnClickListener implements DialogInterface.OnClickListener {
-        public void onClick(DialogInterface dialog, int which) {
-            restart = false;
-            handleDialog();
+
+    public void checkCards(){
+        if(previousCard != null && currentCard != null){
+            if(previousCard.getHeadCardId() != currentCard.getHeadCardId()){
+                previousCard.flipView();
+                currentCard.flipView();
+                cardAdapter.notifyDataSetChanged();
+            }
+            else{
+                previousCard.canFlip(false);
+                currentCard.canFlip(false);
+                if((nbValidCards +=2) == cards.length){
+                    win = true;
+                    showDialog();
+                }
+            }
+            previousCard = null;
+            currentCard = null;
         }
     }
 
-    private final class OkOnClickListener implements DialogInterface.OnClickListener {
-        public void onClick(DialogInterface dialog, int which) {
-            restart = true;
-            handleDialog();
+    private void reset(){
+        for(int i =0;i < cards.length;i++){
+            cards[i].reset();
         }
+        cardAdapter.notifyDataSetChanged();
+        win = false;
+        nbValidCards = 0;
+        previousCard = null;
+        currentCard = null;
     }
 }
